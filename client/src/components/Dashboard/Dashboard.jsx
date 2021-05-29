e9506473a7266bfe4120d7d66a60e5e9703cd152
Ryan Adi Putra <ryannadiputraa@gmail.com>
2021-05-29 11:53:27 +0800

    display customers data with material ui table

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,25 +1,40 @@
+import { CircularProgress, Container, Typography } from "@material-ui/core";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Redirect } from "react-router";
+import DataTable from "../Table/DataTable";
 
 export default function Dashboard() {
     const [redirect, setRedirect] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [tableData, setTableData] = useState([]);
 
     useEffect(() => {
         const fetchCustomers = async () => {
+            setIsLoading(true);
             const res = await fetch("http://localhost:8000/api/customers", {
                 headers: { "Content-Type": "application/json" },
                 credentials: "include",
             });
             const data = await res.json();
+            setTableData(data);
 
             // unauthorized user
             if (data.code === 401) setRedirect(true);
+
+            setIsLoading(false);
         };
         fetchCustomers();
     }, []);
 
     if (redirect) return <Redirect to="/" />;
 
-    return <h1>Dashboard</h1>;
+    return (
+        <Container>
+            <Typography variant="h4" component="h1" align="center" gutterBottom>
+                PEGADAIAN
+            </Typography>
+            {isLoading ? <CircularProgress /> : <DataTable data={tableData} />}
+        </Container>
+    );
 }
